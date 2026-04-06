5af3b234702978be523cd90bf0835249de4daa98
SixtyOne61 <olivier.lavalley@gmail.com>
2026-04-06 23:53:43 +0200

    Can access of hidden section by url

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 
 import FixLeafletIcon from "./components/FixLeafletIcon";
+import { getSectionVisibility } from "./data/loader";
+import { defaultSectionVisibility } from "./data/sections";
 import Home from "./pages/Home";
 import News from "./pages/News";
 import NewsDetail from "./pages/NewsDetail";
@@ -14,20 +16,58 @@ import Hikes from "./pages/Hikes";
 import HikeDetail from "./pages/HikeDetail";
 import Parking from "./pages/Parking";
 
+function AppRoutes() {
+    const [sectionVisibility, setSectionVisibility] = useState(null);
+
+    useEffect(() => {
+        getSectionVisibility().then(setSectionVisibility);
+    }, []);
+
+    if (!sectionVisibility) {
+        return null;
+    }
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+                path="/news"
+                element={sectionVisibility.news ? <News /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/news/:id"
+                element={sectionVisibility.news ? <NewsDetail /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/events"
+                element={sectionVisibility.events ? <Events /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/events/:id"
+                element={sectionVisibility.events ? <EventDetail /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/hikes"
+                element={sectionVisibility.hikes ? <Hikes /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/hikes/:id"
+                element={sectionVisibility.hikes ? <HikeDetail /> : <Navigate to="/" replace />}
+            />
+            <Route
+                path="/parking"
+                element={sectionVisibility.parkings ? <Parking /> : <Navigate to="/" replace />}
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
             <FixLeafletIcon />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/news/:id" element={<NewsDetail />} />
-                <Route path="/events" element={<Events />} />
-                <Route path="/events/:id" element={<EventDetail />} />
-                <Route path="/hikes" element={<Hikes />} />
-                <Route path="/hikes/:id" element={<HikeDetail />} />
-                <Route path="/parking" element={<Parking />} />
-            </Routes>
+            <AppRoutes />
         </BrowserRouter>
     </React.StrictMode>
 );
